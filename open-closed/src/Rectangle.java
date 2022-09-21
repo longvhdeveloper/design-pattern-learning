dfe4db97674b3e8cf1c960b40991f8e0cb8f1072
Vo Hoang Long <vohoanglong07@gmail.com>
2022-09-21 14:45:02 +0700

    Create Rectangle.java

---
+++ open-closed/src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle implements Shape {
+    private final double width;
+    private final double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * length;
+    }
+}
