6f196349df2fe8daf1fc0e8c5af116d73907f41a
Vo Hoang Long <vohoanglong07@gmail.com>
2022-09-21 15:16:11 +0700

    Create Bus.java

---
+++ liskov-subsitution/src/Bus.java
@@ -0,0 +1,15 @@
+public class Bus extends Vehicle{
+    @Override
+    int getSpeed() {
+        return 0;
+    }
+
+    @Override
+    int getCubicCapacity() {
+        return 0;
+    }
+
+    public boolean isHatchBack() {
+        return false;
+    }
+}
